49164172da172c880843eba9164e8da667e0b717
Yulia Azovtseva <yulia.pryania@gmail.com>
2023-11-07 15:53:42 +0400

    Remote link changed

--- src/test/java/com/demoqa/helpers/WebDriverProvider.java
+++ src/test/java/com/demoqa/helpers/WebDriverProvider.java
@@ -6,24 +6,26 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
 import static com.demoqa.config.App.appConfig;
+import static com.codeborne.selenide.Configuration.*;
 
 public class WebDriverProvider {
     public static void webDriverConfigInit() {
-        Configuration.baseUrl = appConfig.webUrl();
-        Configuration.browser = appConfig.browser();
-        Configuration.browserVersion = appConfig.browserVersion();
-        Configuration.browserSize = appConfig.browserSize();
+        pageLoadStrategy = "eager";
+        baseUrl = appConfig.webUrl();
+        browser = appConfig.browser();
+        browserVersion = appConfig.browserVersion();
+        browserSize = appConfig.browserSize();
 
         if (appConfig.isRemote()) {
-            Configuration.remote = appConfig.remoteUrl();
+            remote = "https://user1:1234@" + appConfig.remoteUrl() + "/wd/hub";
             DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("browserName", Configuration.browser);
-            capabilities.setCapability("browserVersion", Configuration.browserVersion);
+            capabilities.setCapability("browserName", browser);
+            capabilities.setCapability("browserVersion", browserVersion);
             capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                     "enableVNC", true,
                     "enableVideo", true
             ));
-            Configuration.browserCapabilities = capabilities;
+            browserCapabilities = capabilities;
         }
     }
 }
